8c9b3494491a32929c2080674c59eb362976c051
jeisson <thedavid147@gmail.com>
2026-01-13 22:53:13 -0500

    Carrito visible con envío a WhatsApp

--- script.js
+++ script.js
@@ -2,7 +2,45 @@ let carrito = [];
 
 function agregarCarrito(nombre, precio, imagen) {
   carrito.push({ nombre, precio, imagen });
+  mostrarCarrito();
+}
 
-  alert(nombre + " agregado al carrito 🛒");
-  console.log(carrito);
+function mostrarCarrito() {
+  const lista = document.getElementById("lista-carrito");
+  const totalSpan = document.getElementById("total");
+
+  lista.innerHTML = "";
+  let total = 0;
+
+  carrito.forEach((producto, index) => {
+    total += producto.precio;
+
+    lista.innerHTML += `
+      <div class="item-carrito">
+        <img src="${producto.imagen}">
+        <div>
+          <p>${producto.nombre}</p>
+          <p>$${producto.precio}</p>
+        </div>
+      </div>
+    `;
+  });
+
+  totalSpan.textContent = total;
+}
+
+function enviarWhatsApp() {
+  let mensaje = "Hola, quiero hacer este pedido:%0A%0A";
+  let total = 0;
+
+  carrito.forEach(p => {
+    mensaje += `- ${p.nombre} ($${p.precio})%0A`;
+    total += p.precio;
+  });
+
+  mensaje += `%0A*Total:* $${total}`;
+
+  const telefono = "573105822406"; // CAMBIA ESTE NÚMERO
+
+  window.open(`https://wa.me/${telefono}?text=${mensaje}`);
 }
